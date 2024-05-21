0cc8f9c789b08536dbcfa4528221328476068f1d
Abhishekk14 <112262227+Abhishekk14@users.noreply.github.com>
2024-05-21 06:29:18 +0000

    create

---
+++ 01_vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+function App() {
+ 
+
+  return (
+    <h1>chai aur react with vite | abhishek</h1>
+  )
+}
+
+export default App
---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+function App() {
+  return (
+  <h1>chai aur code | Abhishek
+  </h1>
+  );
+}
+
+export default App;
